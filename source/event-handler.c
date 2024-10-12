e0088ac19045d1db58599f6bf1ccbb1522850bb0
Moon Yeounsu <yyyynoom@gmail.com>
2024-10-13 00:48:22 +0900

    thread synchronization

--- source/event-handler.c
+++ source/event-handler.c
@@ -15,16 +15,9 @@
 #define MAX_EVENTS 1024
 #define MAX_WORKER 4
 
-typedef enum event_handler_state {
-	EVENT_HANDLER_READY = 0,
-	EVENT_HANDLER_RUNNING,
-	EVENT_HANDLER_STOP,
-
-	EVENT_HANDLER_ERROR,
-} EventHandlerstate;
-
 typedef enum event_worker_state {
-	EVENT_WORKER_READY = 0,
+	EVENT_WORKER_SETUP = 0,
+	EVENT_WORKER_READY,
 	EVENT_WORKER_RUNNING,
 	EVENT_WORKER_ERROR,
 } EventWorkerstate;
@@ -35,9 +28,9 @@ typedef struct event_worker {
 	struct list object_list;
 
 	struct list to_delete;
-	mtx_t lock;
 
 	int nobject;
+	mtx_t lock;
 
 	int epfd;
 	int evfd;
@@ -64,7 +57,10 @@ struct event_handler {
 	EventCallback callback;
 	unsigned int next_worker;
 
-	EventHandlerstate state;
+	struct {
+		cnd_t cond;
+		mtx_t mutex;
+	} signal, barrier;
 };
 
 EventHandler event_handler_create(void)
@@ -77,6 +73,12 @@ EventHandler event_handler_create(void)
 
 	handler->callback = NULL;
 	handler->next_worker = 0;
+
+	cnd_init(&handler->signal.cond);
+	mtx_init(&handler->signal.mutex, mtx_plain);
+
+	cnd_init(&handler->barrier.cond);
+	mtx_init(&handler->barrier.mutex, mtx_plain);
 	
 	return handler;
 
@@ -127,10 +129,10 @@ int event_handler_del(EventHandler handler, EventObject object)
 	return 0;
 }
 
-static void handle_event(EventWorker worker, EventHandler handler,
-			struct epoll_event *event)
+static void handle_event(EventWorker worker, struct epoll_event *event)
 {
 	EventObject object;
+	EventHandler handler = worker->handler;
 
 	if (event->data.ptr == worker) {
 		mtx_lock(&worker->lock);
@@ -155,6 +157,62 @@ static void handle_event(EventWorker worker, EventHandler handler,
 		handler->callback(object);
 }
 
+int event_worker_init(EventWorker worker)
+{
+	worker->epfd = epoll_create1(0);
+	if (worker->epfd == -1)
+		goto RETURN_ERR;
+
+	worker->evfd = eventfd(0, EFD_NONBLOCK);
+	if (worker->evfd == -1)
+		goto CLOSE_EPOLL;
+
+	struct epoll_event event = {
+		.events = EPOLLIN,
+		.data.ptr = worker
+	};
+	if (epoll_ctl(worker->epfd, EPOLL_CTL_ADD, worker->evfd, &event) == -1)
+		goto CLOSE_EVENT;
+	
+	if (mtx_init(&worker->lock, mtx_plain) != thrd_success)
+		goto DELETE_EPOLL;
+
+	list_init_head(&worker->object_list);
+	list_init_head(&worker->to_delete);
+
+	worker->nobject = 0;
+
+	return 0;
+
+DELETE_EPOLL:	epoll_ctl(worker->epfd, EPOLL_CTL_DEL, worker->evfd, NULL);
+CLOSE_EVENT:	close(worker->evfd);
+CLOSE_EPOLL:	close(worker->epfd);
+RETURN_ERR:	return -1;
+}
+
+static void event_worker_destroy(EventWorker worker)
+{
+	mtx_destroy(&worker->lock);
+	epoll_ctl(worker->epfd, EPOLL_CTL_DEL, worker->evfd, NULL);
+	close(worker->evfd);
+	close(worker->epfd);
+}
+
+static void event_worker_barrier(EventWorker worker)
+{
+	EventHandler handler = worker->handler;
+
+	mtx_lock(&handler->barrier.mutex);
+
+	mtx_lock(&handler->signal.mutex);
+	cnd_signal(&handler->signal.cond);
+	mtx_unlock(&handler->signal.mutex);
+
+	cnd_wait(&handler->barrier.cond, &handler->barrier.mutex);
+
+	mtx_unlock(&handler->barrier.mutex);
+}
+
 static int event_worker(void *arg)
 {
 	struct epoll_event events[MAX_EVENTS];
@@ -162,126 +220,98 @@ static int event_worker(void *arg)
 	EventWorker worker = arg;
 	EventHandler handler = worker->handler;
 
-	worker->epfd = epoll_create1(0);
-	if (worker->epfd == -1)
-		goto SET_ERR_STATE;
+	worker->state = EVENT_WORKER_SETUP;
+	if (event_worker_init(worker) == -1)
+		goto SEND_ERR_SIGNAL;
 
-	worker->evfd = eventfd(0, EFD_NONBLOCK);
-	if (worker->evfd == -1)
-		goto CLOSE_EPOLL;
-
-	struct epoll_event event;
-	event.events = EPOLLIN;
-	event.data.ptr = worker;
-	if (epoll_ctl(worker->epfd, EPOLL_CTL_ADD, worker->evfd, &event) == -1)
-		goto CLOSE_EVENT;
-
-	worker->nobject = 0;
-	list_init_head(&worker->object_list);
-	list_init_head(&worker->to_delete);
-
-	if (mtx_init(&worker->lock, mtx_plain) == thrd_error)
-		goto EPOLL_DEL;
-
-	worker->state = EVENT_WORKER_RUNNING;
-
-	while (handler->state == EVENT_HANDLER_READY)
-		sleep(1);
-
-	if (handler->state == EVENT_HANDLER_ERROR)
-		goto DESTROY_MTX;
-
-	while (handler->state == EVENT_HANDLER_RUNNING)
+	worker->state = EVENT_WORKER_READY;
+	event_worker_barrier(worker);
+	
+	while (worker->state == EVENT_WORKER_RUNNING)
 	{
 		int ret = epoll_wait(worker->epfd, events, MAX_EVENTS, -1);
 		if (ret == -1) {
 			if (errno == EINTR)
 				continue;
 
-			goto DESTROY_MTX;
+			goto DESTROY_WORKER;
 		}
 
 		for (int i = 0; i < ret; i++)
-			handle_event(worker, handler, events + i);
+			handle_event(worker, events + i);
 	}
 
 	LIST_FOREACH_ENTRY_SAFE(&worker->object_list, object,
 			 	struct event_object, list)
 	{
 		list_del(&object->list);
-		event_object_destroy(object);
 		worker->nobject--;
 	}
 
-	mtx_destroy(&worker->lock);
-	epoll_ctl(worker->epfd, EPOLL_CTL_DEL, worker->evfd, NULL);
-
-	close(worker->evfd);
-	close(worker->epfd);
+	event_worker_destroy(worker);
 
 	return 0;
 
-DESTROY_MTX:	mtx_destroy(&worker->lock);
-EPOLL_DEL:	epoll_ctl(worker->epfd, EPOLL_CTL_DEL, worker->evfd, NULL);
-CLOSE_EVENT:	close(worker->evfd);
-CLOSE_EPOLL:	close(worker->epfd);
-SET_ERR_STATE:	worker->state = EVENT_WORKER_ERROR;
-RETURN_ERR:	return -1;
-}
-
-static int create_worker(EventHandler handler, EventWorker worker)
-{
-	thrd_t tid;
-
-	worker->state = EVENT_WORKER_READY;
-	worker->handler = handler;
-
-	tid = thrd_create(&worker->tid, event_worker, worker);
-	if (tid != thrd_success)
+DESTROY_WORKER:	event_worker_destroy(worker);
+SEND_ERR_SIGNAL:worker->state = EVENT_WORKER_ERROR;
+		mtx_lock(&handler->signal.mutex);
+		cnd_signal(&handler->signal.cond);
+		mtx_unlock(&handler->signal.mutex);
 		return -1;
-
-	while (worker->state == EVENT_WORKER_READY)
-		sleep(1);
-
-	if (worker->state == EVENT_WORKER_ERROR)
-		return -1;
-
-	return 0;
 }
 
 int event_handler_start(EventHandler handler)
 {
-	handler->state = EVENT_HANDLER_READY;
+	int i;
 
-	for (int i = 0; i < MAX_WORKER; i++) {
+	for (i = 0; i < MAX_WORKER; i++) {
 		EventWorker worker = &handler->worker[i];
 
-		if (create_worker(handler, worker) == -1) {
-			handler->state = EVENT_HANDLER_ERROR;
+		worker->handler = handler;
 
-			for (int j = i - 1; j >= 0; j--) {
-				int retval;
+		mtx_lock(&handler->signal.mutex);
 
-				worker = &handler->worker[j];
-				thrd_join(worker->tid, &retval);
-			}
-
-			return -1;
+		if (thrd_create(&worker->tid, event_worker, worker) != thrd_success) {
+			mtx_unlock(&handler->signal.mutex);
+			i--;
+			goto SEND_ERR_SIGNAL;
 		}
+
+		cnd_wait(&handler->signal.cond, &handler->signal.mutex);
+
+		mtx_unlock(&handler->signal.mutex);
+
+		if (worker->state == EVENT_WORKER_ERROR)
+			goto SEND_ERR_SIGNAL;
+
+		worker->state = EVENT_WORKER_RUNNING;
 	}
 
-	handler->state = EVENT_HANDLER_RUNNING;
+	mtx_lock(&handler->barrier.mutex);
+	cnd_broadcast(&handler->barrier.cond);
+	mtx_unlock(&handler->barrier.mutex);
 
 	return 0;
+
+SEND_ERR_SIGNAL:mtx_lock(&handler->barrier.mutex);
+		cnd_broadcast(&handler->barrier.cond);
+		mtx_unlock(&handler->barrier.mutex);
+		while (i-- >= 0) {
+			EventWorker worker = &handler->worker[i];
+			int ret;
+
+			thrd_join(worker->tid, &ret);
+		}
+		return -1;
 }
 
 int event_handler_stop(EventHandler handler)
 {
-	handler->state = EVENT_HANDLER_STOP;
-
 	for (int i = 0; i < MAX_WORKER; i++) {
 		EventWorker worker = &handler->worker[i];
 		int retval;
+
+		worker->state = EVENT_WORKER_ERROR;
 
 		eventfd_write(worker->evfd, 1);
 		thrd_join(worker->tid, &retval);
@@ -292,6 +322,12 @@ int event_handler_stop(EventHandler handler)
 
 void event_handler_destroy(EventHandler handler)
 {
+	mtx_destroy(&handler->signal.mutex);
+	cnd_destroy(&handler->signal.cond);
+
+	mtx_destroy(&handler->barrier.mutex);
+	cnd_destroy(&handler->barrier.cond);
+
 	free(handler);
 }
 
